9ef4282576dddc7b48ab4cdd4dada6c214c6b093
Hashworks <awadh.kishor@hashworks.co>
2018-04-17 12:32:09 +0530

    add body css 2

---
+++ tmp/js/pages/workpage.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+  // $('.lb-filter-service').hide();
+if(sessionStorage.getItem("pageDetails") != "") {
+  smoothScroll();
+}
+  function smoothScroll(text) {
+    setTimeout(function() {
+      $('html, body').animate({
+        'scrollTop' : $(".lb_caseStusy").position().top
+      });
+      if(sessionStorage.getItem("pageDetails") == "Service Design") {
+        $('#filterli-2').click();
+      } else if(sessionStorage.getItem("pageDetails") == "Product Design") {
+        $('#filterli-1').click();
+      } else if(sessionStorage.getItem("pageDetails") == "Branding &amp; Communication Design") {
+        debugger;
+        $('#filterli-3').click();
+      }
+      sessionStorage.setItem("pageDetails", ""); 
+    }, 500);
+  }
+});
