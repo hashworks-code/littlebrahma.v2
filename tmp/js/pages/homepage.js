d0c8912405654d07eaa7cb869023e518cb8c970b
Hashworks <awadh.kishor@hashworks.co>
2018-04-23 13:27:19 +0530

    content

---
+++ tmp/js/pages/homepage.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+
+//     if(sessionStorage.getItem("Hometab") != "" &&  sessionStorage.getItem("Hometab") != null) {
+//         slowScroll();
+    
+//       }
+      
+    
+//     function slowScroll(){
+//       setTimeout(function() {
+//         $('html, body').animate({
+//           'scrollTop' : $(".lb_work_filter").position().top-150
+//         });
+//         if(sessionStorage.getItem("Hometab") == "Service Design") {
+//             $('#filterli-2').click();
+//           } else if(sessionStorage.getItem("Hometab") == "Product Design") {
+//             $('#filterli-1').click();
+//           } else if(sessionStorage.getItem("Hometab") == "Branding & Comm.") {
+//             $('#filterli-3').click();
+//           }
+//           sessionStorage.setItem("Hometab", ""); 
+//           sessionStorage.setItem("pageDetails", ""); 
+
+//       },500);
+    
+//     }
+// })
