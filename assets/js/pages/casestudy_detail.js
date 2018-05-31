1d90f1315546a706b1d87c463b4c8fbde5792f79
Nikhil__Raina <nikhil.raina@hashworks.com>
2018-05-31 19:05:36 +0530

    bootcamp link done

--- assets/js/pages/casestudy_detail.js
+++ assets/js/pages/casestudy_detail.js
@@ -1,1 +1,30 @@
-$(document).ready(function(){$(".slider-nav").slick({autoplay:!0,autoplaySpeed:1e3,slidesToShow:3,slidesToScroll:1,centerMode:!0,focusOnSelect:!0,responsive:[{breakpoint:767,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]})});
+$(document).ready(function(){
+
+      $('.slider-nav').slick({
+            autoplay: true,
+            autoplaySpeed: 1000,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: true,
+            focusOnSelect: true,
+            responsive: [
+                  {
+                    breakpoint: 767,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2
+                    }
+                  },
+                  {
+                    breakpoint: 480,
+                    settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1
+                    }
+                  }
+                  // You can unslick at a given breakpoint now by adding:
+                  // settings: "unslick"
+                  // instead of a settings object
+                ]
+      });
+  });
--- assets/js/pages/homepage.js
+++ assets/js/pages/homepage.js
@@ -1,1 +1,59 @@
-$(document).ready(function(){""!=sessionStorage.getItem("Hometab")&&null!=sessionStorage.getItem("Hometab")&&function(){setTimeout(function(){$("html, body").animate({scrollTop:$(".lb_caseStusy").position().top-140}),"Service Design"==sessionStorage.getItem("Hometab")?($(".option").text("Service Design"),$("#filterli-2").click()):"Product Design"==sessionStorage.getItem("Hometab")?($("#filterli-1").click(),$(".option").text("Product Design")):"Branding & Comm."==sessionStorage.getItem("Hometab")&&($("#filterli-3").click(),$(".option").text("Branding & Comm.")),sessionStorage.setItem("Hometab",""),sessionStorage.setItem("pageDetails","")},500)}()});
+// $(document).ready(function(){
+
+//     if(sessionStorage.getItem("Hometab") != "" &&  sessionStorage.getItem("Hometab") != null) {
+//         slowScroll();
+    
+//       }
+      
+    
+//     function slowScroll(){
+//       setTimeout(function() {
+//         $('html, body').animate({
+//           'scrollTop' : $(".lb_work_filter").position().top-150
+//         });
+//         if(sessionStorage.getItem("Hometab") == "Service Design") {
+//             $('#filterli-2').click();
+//           } else if(sessionStorage.getItem("Hometab") == "Product Design") {
+//             $('#filterli-1').click();
+//           } else if(sessionStorage.getItem("Hometab") == "Branding & Comm.") {
+//             $('#filterli-3').click();
+//           }
+//           sessionStorage.setItem("Hometab", ""); 
+//           sessionStorage.setItem("pageDetails", ""); 
+
+//       },500);
+    
+//     }
+// })
+
+$(document).ready(function(){
+
+
+
+    if(sessionStorage.getItem("Hometab") != "" &&  sessionStorage.getItem("Hometab") != null) {
+        slowScroll();
+    
+      }
+    function slowScroll() {
+        setTimeout(function() {
+          $('html, body').animate({
+            'scrollTop' : $(".lb_caseStusy").position().top-140
+          });
+          if(sessionStorage.getItem("Hometab") == "Service Design") {
+              $(".option").text('Service Design');
+            $('#filterli-2').click();
+          } else if(sessionStorage.getItem("Hometab") == "Product Design") {
+            $('#filterli-1').click();
+            $(".option").text('Product Design');
+    
+          } else if(sessionStorage.getItem("Hometab") == "Branding & Comm.") {
+            $('#filterli-3').click();
+            $(".option").text('Branding & Comm.');
+    
+          }
+          sessionStorage.setItem("Hometab", ""); 
+    
+          sessionStorage.setItem("pageDetails", ""); 
+        }, 500);
+      }
+    })
